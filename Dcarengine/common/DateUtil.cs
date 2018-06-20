c6809b5161f515580d7f4556c52f124d832fc4fd
luoqingming <850432759@qq.com>
2018-06-20 16:32:13 +0800

    ss

---
+++ Dcarengine/common/DateUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dcarengine.common
+{
+    class DateUtil
+    {
+
+
+        public static String getDate() {
+
+
+            String  date   = DateTime.Now.ToString("yyyyMMdd");
+
+            return date;
+
+
+        }
+
+
+
+
+    }
+}
