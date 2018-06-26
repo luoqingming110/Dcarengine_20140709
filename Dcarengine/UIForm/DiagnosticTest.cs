68e9e771e3b81c352595741823021cc1f9dc3b13
luoqingming <850432759@qq.com>
2018-06-26 18:01:42 +0800

    提交

---
+++ Dcarengine/UIForm/DiagnosticTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Dcarengine.UIForm
+{
+    public partial class DiagnosticTest : Form
+    {
+        public DiagnosticTest()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
